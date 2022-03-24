df877988d0a799bbb04ac1918b5e7d14dbfc5ee0
artur <artur.h.0427@gmail.com>
2022-03-24 22:38:04 +0100

    Install.sh added and synced package files.

---
+++ install.sh
@@ -0,0 +1,3 @@
+## install dependencies
+echo -e "\033[33mInstalling dependencies...\033[0m"
+npm install >&- 2>&- && echo -e "\033[33m \n++++++++++++++++++++++\ndependencies installed\n++++++++++++++++++++++ \033[0m"
